c6801f716f7a73f839dfc88c7cdd2d00b3de84a4
Silva <darlan.ricardo.silva@accenture.com>
2019-08-12 13:11:48 -0300

    Configurando Redis no spring

--- src/main/java/br/com/darlan/springmongo/SpringMongoApplication.java
+++ src/main/java/br/com/darlan/springmongo/SpringMongoApplication.java
@@ -2,8 +2,10 @@ package br.com.darlan.springmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringMongoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/darlan/springmongo/service/ClienteService.java
+++ src/main/java/br/com/darlan/springmongo/service/ClienteService.java
@@ -3,6 +3,7 @@ package br.com.darlan.springmongo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import br.com.darlan.springmongo.domain.Cliente;
@@ -14,6 +15,7 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
+	@Cacheable(cacheNames = "clientes", key = "#root.method.name")
 	public List<Cliente> findAll(){
 		return repo.findAll();
 	}
