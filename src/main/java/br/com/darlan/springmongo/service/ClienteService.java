6fa7a726f88efe4ca4932787850b94b24ce4ef2e
Silva <darlan.ricardo.silva@accenture.com>
2019-08-12 14:48:43 -0300

    Inserindo cliente e invalidando cache do redis

--- src/main/java/br/com/darlan/springmongo/service/ClienteService.java
+++ src/main/java/br/com/darlan/springmongo/service/ClienteService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import br.com.darlan.springmongo.domain.Cliente;
+import br.com.darlan.springmongo.dto.ClienteDTO;
 import br.com.darlan.springmongo.repository.ClienteRepository;
 import br.com.darlan.springmongo.service.exception.ObjectNotFoundException;
 
@@ -26,5 +28,15 @@ public class ClienteService {
 		System.out.println(id);
 		Optional<Cliente> cliente = repo.findById(id);
 		return cliente.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	@CacheEvict(cacheNames = "clientes", allEntries = true)
+	public Cliente insert (Cliente obj) {
+		return repo.insert(obj);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), 
+							objDTO.getCpf(), objDTO.getDataNascimento());
 	}
 }
